a17363392dfab9499325d9fe9c450aa9e5e5b0ab
liuyuchen777 <liuyuchen.hust@gmail.com>
2021-12-26 14:03:16 +0900

    start repo

---
+++ src/main/java/com/java/basketmall/BasketMallApplication.java
@@ -0,0 +1,14 @@
+package com.java.basketmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketMallApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BasketMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/basketmall/BasketMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.basketmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
